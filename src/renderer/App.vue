1938e2e498e749f1104d40b7159affdb0a777900
R-Imai <ryohsuke.imai@gmail.com>
2018-09-24 19:22:44 +0900

    init commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue-clock'
+  }
+</script>
+
+<style>
+  body {
+    -webkit-app-region: drag;
+    width: 300px;
+    height: 200px;
+  }
+
+</style>
